dffd3ca59af737d7d40dd6d2564495d07268b05b
Filip Buric <29886463+fburic@users.noreply.github.com>
2020-10-30 20:21:27 +0100

    refactor: Make cluster script use Singularity by default

--- scripts/parafac/decompose_cluster.sh
+++ scripts/parafac/decompose_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --ntasks 1
+#SBATCH --ntasks 8
 #SBATCH --time 168:00:00
 #SBATCH --output='decompose-%j.out'
 #SBATCH --error='decompose-%j.err'
@@ -14,15 +14,14 @@ fi
 # A single GPU is used for each array job, on which parallel decompositions will be run
 N_PAR_DECOMP=$2
 
-CUDA_VISIBLE_DEVICES=0
+# Working on a shared machine, so careful to use a unique MPS pipe
+pipe_id=$(uuidgen)
 
-source scripts/parafac/start_mps.sh
-
-# Meant to be run as an array job.
-# The number of partitions is that of the number of array jobs.
-# Each job in the array is assigned a data partition using the array task ID.
-snakemake -j ${N_PAR_DECOMP} --keep-going -s scripts/parafac/decompose_parafac.Snakefile \
-  --config npartitions=${SLURM_ARRAY_TASK_COUNT} partition=${SLURM_ARRAY_TASK_ID} device=0 \
-  --configfile $1
-
-source scripts/parafac/stop_mps.sh
+singularity exec --nv candia.sif /bin/bash -c \
+  "CUDA_VISIBLE_DEVICES=0 \
+  && source scripts/parafac/start_mps.sh ${pipe_id} \
+  && snakemake -j ${N_PAR_DECOMP} --nolock --forceall --keep-going \
+  -s scripts/parafac/decompose_parafac.Snakefile \
+  --config npartitions=${SLURM_ARRAY_TASK_COUNT} partition=${SLURM_ARRAY_TASK_ID} pipe_id=${pipe_id} \
+  --configfile $1 \
+  && source scripts/parafac/stop_mps.sh"
